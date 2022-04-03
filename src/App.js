caf646d4267b6b924fddc2f76a36c889f83dc613
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-04-03 19:41:05 +0530

    Adding Networks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,162 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const Polygon = async() => {
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: '0x89',
+            chainName: 'Polygon Mainnet',
+            rpcUrls: ['https://rpc-mainnet.matic.network'],
+          },
+        ],
+      });
+    } catch (e) {
+      alert(e.message)
+    }
+  }
+
+  const Mumbai = async() => {
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: '0x13881',
+            chainName: 'Mumbai Test Network',
+            rpcUrls: ['https://rpc-mumbai.matic.today'],
+          },
+        ],
+      });
+    } catch (e) {
+      alert(e.message)
+    }
+  }
+
+  const Ethereum = async() => {
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: '0x1',
+            chainName: 'Ethereum Mainnet',
+            rpcUrls: ['https://mainnet.infura.io/v3/'],
+          },
+        ],
+      });
+    } catch (e) {
+      alert(e.message)
+    }
+  }
+
+  const Rinkeby = async() => {
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: '0x4',
+            chainName: 'Rinkeby Test Network',
+            rpcUrls: ['https://rinkeby.infura.io/v3/'],
+          },
+        ],
+      });
+    } catch (e) {
+      alert(e.message)
+    }
+  }
+
+  const Kovan = async() => {
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: '0x2a',
+            chainName: 'Kovan Test Network',
+            rpcUrls: ['https://kovan.infura.io/v3/'],
+          },
+        ],
+      });
+    } catch (e) {
+      alert(e.message)
+    }
+  }
+
+  const Ropsten = async() => {
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: '0x3',
+            chainName: 'Ropsten Test Network',
+            rpcUrls: ['https://ropsten.infura.io/v3/'],
+          },
+        ],
+      });
+    } catch (e) {
+      alert(e.message)
+    }
+  }
+
+  const Goerli = async() => {
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: '0x5',
+            chainName: 'Goerli Test Network',
+            rpcUrls: ['https://goerli.infura.io/v3/'],
+          },
+        ],
+      });
+    } catch (e) {
+      alert(e.message)
+    }
+  }
+
+  const Sokol = async() => {
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: '0x4d',
+            chainName: 'SOKOL POA Test Network',
+            rpcUrls: ['https://sokol.poa.network'],
+          },
+        ],
+      });
+    } catch (e) {
+      alert(e.message)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Add Networks to your wallet</h1>
+
+      <button onClick={Polygon}>Add Polygon Main Network</button>
+      <button onClick={Mumbai}>Add Mumbai Test Network</button>
+      <button onClick={Ethereum}>Add Ethereum Main Network</button>
+      <button onClick={Kovan}>Add Kovan Test Network</button>
+      <button onClick={Rinkeby}>Add Rinkeby Test Network</button>
+      <button onClick={Goerli}>Add Goerli Test Network</button>
+      <button onClick={Ropsten}>Add Ropsten Test Network</button>
+      <button onClick={Sokol}>Add SOKOL POA Test Network</button>
     </div>
   );
 }
